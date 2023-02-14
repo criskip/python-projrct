9f5ff5b7b857442009275f6a82dece08869b5840
eMobilis <preciousmwangi34@gmail.com>
2023-02-14 13:33:51 +0300

    object oriented programmin

---
+++ OOP/one.py
@@ -0,0 +1,40 @@
+#class House():
+   # def __init__(self, type, location, owner):
+
+  #    self.type = type
+   #     self.location = location
+    #    self.owner = owner
+
+#owner_one = House("Bungalow", "Kitengela", "sharon" )
+#print(owner_one.type)
+#print(owner_one.location)
+#print(owner_one.owner)
+#print("------end of owner_one")
+
+#owner_two = House("Penthouse", "Kilimani", "caroline")
+#print(owner_two.owner)
+#print(owner_two.location)
+#print(owner_two.type)
+
+
+class Car():
+    def __init__(self,type, numberplate, owner):
+
+        self.type = type
+        self.numberplate = numberplate
+        self.owner = owner
+
+owner1 = Car("Mercedes", "KDA467k", "Kemboi")
+print(owner1.type)
+print(owner1.numberplate)
+print(owner1.owner)
+print("-----end of owner1")
+
+
+owner2 = Car("Prado", "KDD654P","Kip" )
+print(owner2.type)
+print(owner2.numberplate)
+print(owner2.owner)
+print("-----end of owner 2")
+
+
